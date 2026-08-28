599f82c7fa1d883cfdfc4e6bd0c793f2547050ca
Yaoxing Zhang <yaoxing.zhang@gmail.com>
2026-08-28 16:30:29 +0200

    chore: normalize blank lines after copyright header (ruff format)

--- src/mongo_x_ray_hc/__init__.py
+++ src/mongo_x_ray_hc/__init__.py
@@ -9,4 +9,3 @@ THIS MATERIAL IS PROVIDED "AS IS" WITHOUT WARRANTY OR LIABILITY.
 
 MongoDB health check analysis package.
 """
-
--- tests/parsers/__init__.py
+++ tests/parsers/__init__.py
@@ -9,4 +9,3 @@ THIS MATERIAL IS PROVIDED "AS IS" WITHOUT WARRANTY OR LIABILITY.
 
 Test package for x-ray project.
 """
-
